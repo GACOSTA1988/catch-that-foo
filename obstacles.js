5d7b7c5094180d41f21740ff7f7aa0fc29682a3b
GACOSTA1988 <georgeacostar@gmail.com>
2021-01-07 14:16:45 -0800

    add obstavles class

---
+++ obstacles.js
@@ -0,0 +1,32 @@
+const obstablesArray = []
+
+ class Obstacle {
+   constructor(){
+     this.top = (Math.random() * canvas.height/3) + 20
+     this.bottom = (Math.random() * canvas.height/3) + 20
+     this.x = canvas.width 
+     this.width = 20
+     this.color = 'hsla('+ hue +', 100%, 50%, 0.8)' 
+    }
+    draw(){
+      ctx.fillStyle = this.color
+      ctx.fillRect(this.x, 0, this.width, this.top)
+      ctx.fillRect(this.x, canvas.height - this.bottom, this.width, this.bottom)
+    }
+    update(){
+      this.x -= gamespeed
+      this.draw()
+    }
+ } 
+
+ function handleObstacles(){
+   if (frame%50 === 0){
+     obstablesArray.unshift(new Obstacle)
+   }
+   for (let i = 0; i < obstablesArray.length; i++){
+     obstablesArray[i].update()
+   }
+   if (obstablesArray.length > 20){
+     obstablesArray.pop(obstablesArray[0])
+   }
+ }
